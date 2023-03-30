0a3daa16959b1aa0dc8b8aaf56bf1fbee1d7071a
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-30 10:24:27 +0200

    Update memory.py
    added Pinecone

--- task_management_system/utils/memory.py
+++ task_management_system/utils/memory.py
@@ -1,26 +1,62 @@
 # filename: memory.py
-# description: A simple memory class to store and retrieve task/result pairs.
+# description: A memory class using Pinecone vector database service to store and retrieve task/result pairs with priority and role.
+
+import os
+import json
+import pinecone
+
+pinecone.deinit()
+pinecone.init(api_key=os.environ["PINECONE_API_KEY"])
+pinecone.create_index(index_name="task_management_memory", metric="euclidean")
 
 class Memory:
     def __init__(self):
-        self._memory_store = {}
+        self.index_name = "task_management_memory"
 
-    def store(self, task, result):
-        """Store a task/result pair in memory."""
+    def _vectorize_task_result(self, task, result, priority, role):
+        # Implement the logic to convert the task, result, priority, and role into a single vector
+        # This will depend on the structure of your tasks and results
+        task_result_data = {
+            "task": task,
+            "result": result,
+            "priority": priority,
+            "role": role
+        }
+        task_result_str = json.dumps(task_result_data)
+        # Convert the string to a vector (use your preferred method for vectorization)
+        pass
+
+    def _devectorize_task_result(self, vector):
+        # Implement the logic to convert the vector back into a task, result, priority, and role
+        # This will depend on the structure of your tasks and results
+        # Convert the vector back to a string (use your preferred method for devectorization)
+        task_result_str = ...
+        task_result_data = json.loads(task_result_str)
+        task = task_result_data["task"]
+        result = task_result_data["result"]
+        priority = task_result_data["priority"]
+        role = task_result_data["role"]
+        return task, result, priority, role
+
+    def store(self, task, result, priority, role):
+        """Store a task/result pair with priority and role in memory."""
         task_id = task["task_id"]
-        self._memory_store[task_id] = {"task": task, "result": result}
+        vector = self._vectorize_task_result(task, result, priority, role)
+        pinecone.upsert(index_name=self.index_name, ids=[task_id], vectors=[vector])
 
     def retrieve(self, task_id):
-        """Retrieve the task/result pair for a given task_id."""
-        task_result_pair = self._memory_store.get(task_id)
-        if task_result_pair:
-            return task_result_pair["task"], task_result_pair["result"]
-        return None, None
+        """Retrieve the task/result pair with priority and role for a given task_id."""
+        vector = pinecone.fetch(index_name=self.index_name, ids=[task_id])
+        if task_id in vector:
+            task, result, priority, role = self._devectorize_task_result(vector[task_id])
+            return task, result, priority, role
+        return None, None, None, None
 
-    def get_all_tasks(self):
-        """Retrieve all tasks stored in memory."""
-        return [task_result_pair["task"] for task_result_pair in self._memory_store.values()]
+    def delete(self, task_id):
+        """Delete a task/result pair with priority and role for a given task_id."""
+        pinecone.delete(index_name=self.index_name, ids=[task_id])
 
-    def get_all_results(self):
-        """Retrieve all results stored in memory."""
-        return [task_result_pair["result"] for task_result_pair in self._memory_store.values()]
+    def deinit(self):
+        """Deinitialize Pinecone and delete the index."""
+        pinecone.deinit()
+        pinecone.delete_index(index_name=self.index_name)
