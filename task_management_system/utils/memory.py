9dd5c52e42f295a0b8c40ce1cf74fab7b6618692
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-30 12:20:08 +0200

    Update memory.py
    updated to fit for the plugin from steamship

--- task_management_system/utils/memory.py
+++ task_management_system/utils/memory.py
@@ -1,57 +1,38 @@
-# filename: memory.py
-# description: This module provides a Memory class to store and retrieve task/result pairs.
+# filename: execution_agent.py
+# description: Processes tasks based on roles and interacts with memory.
 
-import json
-from services.pinecone_integration import memory as pinecone
+from utils.memory import Memory
 
-class Memory:
-    def __init__(self):
-        self.index_name = "task_management_memory"
+# Dictionary to map roles to their corresponding function
+role_to_function_map = {
+    "gpt4_execution_agent": gpt4_execution_agent,
+    "gpt3_5_agent_1": gpt3_5_agent_1,
+    # Add other role-function mappings here
+}
 
-    def _vectorize_task_result(self, task, result, priority, role):
-        # Implement the logic to convert the task, result, priority, and role into a single vector
-        # This will depend on the structure of your tasks and results
-        task_result_data = {
-            "task": task,
-            "result": result,
-            "priority": priority,
-            "role": role
-        }
-        task_result_str = json.dumps(task_result_data)
-        # Convert the string to a vector (use your preferred method for vectorization)
-        pass
+# Initialize memory
+api_key = "your_steamship_api_key"
+memory = Memory(api_key=api_key)
 
-    def _devectorize_task_result(self, vector):
-        # Implement the logic to convert the vector back into a task, result, priority, and role
-        # This will depend on the structure of your tasks and results
-        # Convert the vector back to a string (use your preferred method for devectorization)
-        task_result_str = ...
-        task_result_data = json.loads(task_result_str)
-        task = task_result_data["task"]
-        result = task_result_data["result"]
-        priority = task_result_data["priority"]
-        role = task_result_data["role"]
-        return task, result, priority, role
+while task_queue:
+    # ...
+    current_task = task_queue.pop(0)  # Retrieve the next task from the queue
+    result = None
 
-    def store(self, task, result, priority, role):
-        """Store a task/result pair with priority and role in memory."""
-        task_id = task["task_id"]
-        vector = self._vectorize_task_result(task, result, priority, role)
-        pinecone.upsert(index_name=self.index_name, ids=[task_id], vectors=[vector])
+    # Execute the task based on the type and role
+    function_to_execute = role_to_function_map.get(f"{current_task['role']}_{current_task['type']}")
 
-    def retrieve(self, task_id):
-        """Retrieve the task/result pair with priority and role for a given task_id."""
-        vector = pinecone.fetch(index_name=self.index_name, ids=[task_id])
-        if task_id in vector:
-            task, result, priority, role = self._devectorize_task_result(vector[task_id])
-            return task, result, priority, role
-        return None, None, None, None
+    try:
+        if function_to_execute:
+            context = memory.retrieve(current_task["task_id"])
+            result = function_to_execute(current_task["task"], context)
+            memory.store(task=current_task["task"], result=result, priority=current_task["priority"], role=current_task["role"])
+        else:
+            print(f"No function found for role '{current_task['role']}' and type '{current_task['type']}'")
+    except Exception as e:
+        print(f"An error occurred while executing the task with role '{current_task['role']}' and type '{current_task['type']}': {e}")
+        continue
 
-    def delete(self, task_id):
-        """Delete a task/result pair with priority and role for a given task_id."""
-        pinecone.delete(index_name=self.index_name, ids=[task_id])
-
-    def deinit(self):
-        """Deinitialize Pinecone and delete the index."""
-        pinecone.deinit()
-        pinecone.delete_index(index_name=self.index_name)
+    # Process the result and add new tasks to the queue
+    if result:
+        print("Task result:", result)
