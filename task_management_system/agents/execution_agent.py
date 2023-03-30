5f9885be95498acb12e49b84e35dd655d27cf8e6
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-30 13:34:14 +0200

    Update execution_agent.py
    changed location of the while loop to the main function.

--- task_management_system/agents/execution_agent.py
+++ task_management_system/agents/execution_agent.py
@@ -11,26 +11,3 @@ role_to_function_map = {
     "gpt3_5_agent_1": gpt3_5_agent_1,
     # Add other role-function mappings here
 }
-
-while task_queue:
-    # ...
-    current_task = task_queue.pop(0)  # Retrieve the next task from the queue
-    result = None
-
-    # Execute the task based on the type and role
-    function_to_execute = role_to_function_map.get(f"{current_task['role']}_{current_task['type']}")
-
-    try:
-        if function_to_execute:
-            task, context, priority, role = memory.retrieve(current_task["task_id"])
-            result = function_to_execute(task, context)
-            memory.store(task=task, result=result, priority=priority, role=role)
-        else:
-            print(f"No function found for role '{current_task['role']}' and type '{current_task['type']}'")
-    except Exception as e:
-        print(f"An error occurred while executing the task with role '{current_task['role']}' and type '{current_task['type']}': {e}")
-        continue
-
-    # Process the result and add new tasks to the queue
-    if result:
-        print("Task result:", result)
