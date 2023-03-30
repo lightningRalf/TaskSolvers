8973208c2a685bb605319a3cb15f5c57641da7ed
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-30 06:24:04 +0200

    Update steamship_integration.py
    Simlified code for the instance handle creation

--- task_management_system/services/steamship_integration.py
+++ task_management_system/services/steamship_integration.py
@@ -1,47 +1,47 @@
 from steamship import Steamship
 from api import CustomSteamshipPackage
-from config import shared_instance_config, specific_traits
+from config import instances, LLM_model, specific_traits
 
 steamship_api_key = "your_steamship_api_key"
-gpt4_package_name = "your_deployed_gpt4_package_name"
-gpt3_5_package_name = "your_deployed_gpt3_5_package_name"
 
 client = Steamship(api_key=steamship_api_key)
 
-def create_gpt4_instance(instance_handle, custom_parameters):
-    instance = Steamship.use(gpt4_package_name, instance_handle, config=shared_instance_config)
-    instance.update_parameters(parameters=custom_parameters)
-    return instance
-
-def create_gpt3_5_instance(instance_handle):
-    instance = Steamship.use(gpt3_5_package_name, instance_handle, config=shared_instance_config)
-    instance.update_parameters(parameters=custom_parameters)
+def create_instance(instance_handle, model_type, custom_parameters):
+    instance = client.use(model_type, instance_handle, config=custom_parameters)
     return instance
 
 instances = {}
-for instance_handle, custom_parameters in specific_traits.items():
-    instances[instance_handle] = create_gpt4_instance(instance_handle, custom_parameters)
+
+# Create GPT-4 instances
+for instance_handle, instance_config in LLM_model["gpt4"].items():
+    model_type = "gpt4"
+    instance = create_instance(instance_handle, model_type, instance_config)
+    instances[instance_handle] = instance
+
+# Create GPT-3.5 instances
+for instance_handle, instance_config in LLM_model["gpt3_5"].items():
+    model_type = "gpt3_5"
+    instance = create_instance(instance_handle, model_type, instance_config)
+    instances[instance_handle] = instance
 
 # Access the instances using their handles, e.g.:
 execution_agent_instance = instances["gpt4-execution-agent-instance"]
 task_creation_agent_instance = instances["gpt4-task-creation-agent-instance"]
 task_prioritization_agent_instance = instances["gpt4-task-prioritization-agent-instance"]
 
-gpt3_5_agent_instance_1 = instance["gpt3-5-agent-instance-1"]
-gpt3_5_agent_instance_2 = instance["gpt3-5-agent-instance-2"]
-gpt3_5_agent_instance_3 = instance["gpt3-5-agent-instance-3"]
+gpt3_5_agent_instance_1 = instances["gpt3-5-agent-instance-1"]
+gpt3_5_agent_instance_2 = instances["gpt3-5-agent-instance-2"]
+gpt3_5_agent_instance_3 = instances["gpt3-5-agent-instance-3"]
 
 # Initialize LangChain OpenAI instances for GPT-4 instances
-langchain_openai_gpt4_execution_agent = LangChainOpenAI(client=instances["execution-agent-instance"])
-langchain_openai_gpt4_task_creation_agent = LangChainOpenAI(client=instances["task-creation-agent-instance"])
-langchain_openai_gpt4_task_prioritization_agent = LangChainOpenAI(client=instances["task-prioritization-agent-instance"])
+langchain_openai_gpt4_execution_agent = LangChainOpenAI(client=instances["gpt4-execution-agent-instance"])
+langchain_openai_gpt4_task_creation_agent = LangChainOpenAI(client=instances["gpt4-task-creation-agent-instance"])
+langchain_openai_gpt4_task_prioritization_agent = LangChainOpenAI(client=instances["gpt4-task-prioritization-agent-instance"])
 
 # Initialize LangChain OpenAI instances for GPT-3.5 instances
-langchain_openai_gpt3_5_1 = LangChainOpenAI(client=instances["gpt3_5_agent_instance_1"])
-langchain_openai_gpt3_5_2 = LangChainOpenAI(client=instances["gpt3_5_agent_instance_2"])
-langchain_openai_gpt3_5_3 = LangChainOpenAI(client=instances["gpt3_5_agent_instance_3"])
-
-
+langchain_openai_gpt3_5_1 = LangChainOpenAI(client=instances["gpt3-5-agent-instance-1"])
+langchain_openai_gpt3_5_2 = LangChainOpenAI(client=instances["gpt3-5-agent-instance-2"])
+langchain_openai_gpt3_5_3 = LangChainOpenAI(client=instances["gpt3-5-agent-instance-3"])
 
 def gpt4_execution_agent(input_text):
     response = execution_agent_instance.post("execute_task", input_text=input_text)
@@ -57,15 +57,6 @@ def gpt4_task_prioritization_agent(input_text):
     response = task_prioritization_agent_instance.post("prioritize_tasks", input_text=input_text)
     prioritized_tasks = response["result"]
     return prioritized_tasks
-
-def create_gpt3_5_instance(custom_parameters, instance_handle):
-    instance = Steamship.use("your_deployed_gpt3_5_package_name", instance_handle, config=custom_parameters)
-    return instance
-
-# Customize the instances based on their specific roles
-gpt3_5_instance_handle = "gpt3-5-shared-instance-handle"
-gpt3_5_shared_instance_config = {"traits": "balanced"}
-
 
 def gpt3_5_agent_1(input_text):
     response = gpt3_5_agent_instance_1.post("execute_task", input_text=input_text)
