322da7fdd6d755c748a17a9818c16aa0c278eb70
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-30 09:21:22 +0200

    Update config.py
    updated description and model naming

--- task_management_system/config.py
+++ task_management_system/config.py
@@ -1,14 +1,18 @@
+# config.py
+# Description: Stores and characterizes the different LLMs that will be used
+
 LLM_model = {
     "gpt4": {
         "gpt4-execution-agent-instance": {"role": "execution_agent", "traits": "focused", "other_parameter": "custom_value"},
         "gpt4-task-creation-agent-instance": {"role": "task_creation_agent", "traits": "creative", "other_parameter": "custom_value"},
         "gpt4-task-prioritization-agent-instance": {"role": "task_prioritization_agent", "traits": "analytical", "other_parameter": "custom_value"},
-        "gpt4-text-generation-agent-instance": {"role": "text_generation_agent", "traits": "expressive", "other_parameter": "custom_value"},
-        "gpt4-text-analysis-agent-instance": {"role": "text_analysis_agent", "traits": "insightful", "other_parameter": "custom_value"},
+       # "gpt4-text-generation-agent-instance": {"role": "text_generation_agent", "traits": "expressive", "other_parameter": "custom_value"},
+       # "gpt4-text-analysis-agent-instance": {"role": "text_analysis_agent", "traits": "insightful", "other_parameter": "custom_value"},
     },
     "gpt3_5": {
-        "gpt3-5-agent-instance-1": {"role": "gpt3_5_agent", "traits": "focused", "other_parameter": "custom_value"},
-        "gpt3-5-agent-instance-2": {"role": "gpt3_5_agent", "traits": "creative", "other_parameter": "custom_value"},
-        "gpt3-5-agent-instance-3": {"role": "gpt3_5_agent", "traits": "analytical", "other_parameter": "custom_value"}
+        "gpt3-5-quality-assurance-engineer-instance": {"role": "lead-quality-assurance-engineer", "traits": "focused", "other_parameter": "custom_value"},
+        "gpt3-5-software-engineer-instance": {"role": "software-engineer", "traits": "creative", "other_parameter": "custom_value"},
+        "gpt3-5-software-engineer-instance": {"role": "software-engineer", "traits": "creative", "other_parameter": "custom_value"},
+        "gpt3-5-cybersecurity-specialist-instance": {"role": "cybersecurity-specialist", "traits": "analytical", "other_parameter": "custom_value"}
     }
 }
