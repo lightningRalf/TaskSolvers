b59b623e89020a15b99a60f7714dd7ece07411ec
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-30 12:28:55 +0200

    Update memory.py
    something went wrong, copy pasted wrong text.

--- task_management_system/utils/memory.py
+++ task_management_system/utils/memory.py
@@ -1,38 +1,35 @@
-# filename: execution_agent.py
-# description: Processes tasks based on roles and interacts with memory.
+# filename: memory.py
+# description: This module provides a Memory class to store and retrieve task/result pairs using the Steamship API.
 
-from utils.memory import Memory
+import json
+from steamship_package.src.api import steamship_api_key
+from steamship_package.src import Steamship
 
-# Dictionary to map roles to their corresponding function
-role_to_function_map = {
-    "gpt4_execution_agent": gpt4_execution_agent,
-    "gpt3_5_agent_1": gpt3_5_agent_1,
-    # Add other role-function mappings here
-}
+class Memory:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.steamship = Steamship(api_key=self.api_key)
 
-# Initialize memory
-api_key = "your_steamship_api_key"
-memory = Memory(api_key=api_key)
+    def store(self, task, result, priority, role):
+        """Store a task/result pair with priority and role in memory."""
+        task_id = task["task_id"]
+        task_data = {
+            "task": task,
+            "result": result,
+            "priority": priority,
+            "role": role
+        }
+        self.steamship.set(task_id, json.dumps(task_data))
 
-while task_queue:
-    # ...
-    current_task = task_queue.pop(0)  # Retrieve the next task from the queue
-    result = None
+    def retrieve(self, task_id):
+        """Retrieve the task/result pair with priority and role for a given task_id."""
+        task_data_str = self.steamship.get(task_id)
+        if task_data_str:
+            task_data = json.loads(task_data_str)
+            return task_data["task"], task_data["result"], task_data["priority"], task_data["role"]
+        return None, None, None, None
 
-    # Execute the task based on the type and role
-    function_to_execute = role_to_function_map.get(f"{current_task['role']}_{current_task['type']}")
+    def delete(self, task_id):
+        """Delete a task/result pair with priority and role for a given task_id."""
+        self.steamship.delete(task_id)
 
-    try:
-        if function_to_execute:
-            context = memory.retrieve(current_task["task_id"])
-            result = function_to_execute(current_task["task"], context)
-            memory.store(task=current_task["task"], result=result, priority=current_task["priority"], role=current_task["role"])
-        else:
-            print(f"No function found for role '{current_task['role']}' and type '{current_task['type']}'")
-    except Exception as e:
-        print(f"An error occurred while executing the task with role '{current_task['role']}' and type '{current_task['type']}': {e}")
-        continue
-
-    # Process the result and add new tasks to the queue
-    if result:
-        print("Task result:", result)
