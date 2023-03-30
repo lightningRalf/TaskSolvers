7b88ebe44a572c233821726d25a060b6f113cecb
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-30 10:08:36 +0200

    Update execution_agent.py
    Processes tasks based on roles.

--- task_management_system/agents/execution_agent.py
+++ task_management_system/agents/execution_agent.py
@@ -1,16 +1,30 @@
-from services.steamship_integration import gpt4_generate_text
+# filename: task_processing_agent.py
+# description: Processes tasks based on roles.
 
-def execute_task(task):
-    task_type, task_data = task
+# Dictionary to map roles to their corresponding function
+role_to_function_map = {
+    "gpt4_execution_agent": gpt4_execution_agent,
+    "gpt3_5_agent_1": gpt3_5_agent_1,
+    # Add other role-function mappings here
+}
+
+while task_queue:
+    # ...
+    current_task = task_queue.pop(0)  # Retrieve the next task from the queue
+    result = None
+
+    # Execute the task based on the type and role
+    function_to_execute = role_to_function_map.get(f"{current_task['role']}_{current_task['type']}")
 
     try:
-        if task_type == "gpt4_generate_text":
-            input_text = task_data
-            generated_text = gpt4_generate_text(input_text)
-            return generated_text
+        if function_to_execute:
+            result = function_to_execute(current_task["task"])
         else:
-            print(f"Unknown task type: {task_type}")
-            return None
+            print(f"No function found for role '{current_task['role']}' and type '{current_task['type']}'")
     except Exception as e:
-        print(f"An error occurred while executing the task: {e}")
-        return None
+        print(f"An error occurred while executing the task with role '{current_task['role']}' and type '{current_task['type']}': {e}")
+        continue
+
+    # Process the result and add new tasks to the queue
+    if result:
+        print("Task result:", result)
